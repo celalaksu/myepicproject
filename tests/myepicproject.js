568ad035d89b3ef804c3cd258b60d3666d603585
Celal AKSU <celalaksu@gmail.com>
2022-10-18 15:02:56 +0300

    first commit

---
+++ tests/myepicproject.js
@@ -0,0 +1,75 @@
+const anchor = require("@project-serum/anchor");
+
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
+const main = async () => {
+  console.log("🚀 Starting test...");
+
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicproject;
+
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Call add_gif!
+  // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
+  await program.rpc.addGif("insert_a_giphy_link_here", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log("👀 GIF Count", account.totalGifs.toString());
+
+  // Access gif_list on the account!
+  console.log("👀 GIF List", account.gifList);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+// const anchor = require("@project-serum/anchor");
+
+// describe("myepicproject", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Myepicproject;
+//     const tx = await program.methods.start_stuff_off().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
